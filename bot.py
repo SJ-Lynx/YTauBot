9353d980e4341ba542082009f146b9156b3e0dab
SJ-Lynx <88457845+SJ-Lynx@users.noreply.github.com>
2021-10-30 23:00:42 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "2017103971:AAE7-Uv5dIMhwitp_tjfZGeb0MckW4wknO0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", "6"))
 API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 
